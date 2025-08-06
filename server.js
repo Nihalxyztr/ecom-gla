5c622429021d7a792fbf7ca8ad728fb1a184d039
Nihalxyztr <tripathinihal2006@gmail.com>
2025-08-06 09:43:55 +0530

    ecom web

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const { log } = require("console")
-const app =require("./src/app")
-const connect=require("./src/db/db")
+const app = require("./src/app");
+const connect = require("./src/db/db");
 
+const PORT = process.env.PORT || 3000;
 
-const PORT=process.env.PORT
-
-app.listen(PORT,()=>{
-    console.log("Server is running on port no:",PORT);
-    connect()
-})
+connect().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server is running on port no:", PORT);
+    });
+}).catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+});
